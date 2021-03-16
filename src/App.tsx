ad182a86b6e330883b95fb72b621119ac9f07909
NicholasNeto <nicholasneto@outlook.com>
2021-03-16 19:38:55 -0300

    add img in Component Caroseul

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 
+import { Caroseul } from './components/Caroseul';
+
 export function App() {
   return (
     <div className="App">
       <h1>Thalyta Braga</h1>
+      <Caroseul />
     </div>
   );
 }
---
+++ src/components/Caroseul.tsx
@@ -0,0 +1,10 @@
+import logo from '../imagens/trilho.png'
+
+export function Caroseul (){
+    return ( 
+        <div>
+            <img src={logo} alt=""/>
+            <button>Proximo</button>
+        </div>
+     )
+}
